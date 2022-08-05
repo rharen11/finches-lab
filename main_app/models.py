27c599db71c96fa4765235441c33aeaf8fd1a19f
Rachel Kinzel <rachel.l.kinzel@gmail.com>
2022-08-05 13:56:50 -0700

    rock index page

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Rock(models.Model):
+  type = models.CharField(max_length=100)
+  color = models.CharField(max_length=50)
+  description = models.TextField(max_length=300)
+  location = models.CharField(max_length=50)
+
+  def __str__(self):
+    return self.type
--- main_app/views.py
+++ main_app/views.py
@@ -1,23 +1,12 @@
 from django.shortcuts import render
-
-from django.http import HttpResponse
+from .models import Rock
 
 def home(request):
-  return HttpResponse('<h1>Finches</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
 
-class Rock:
-  def __init__(self, color, description, location):
-    self.color = color
-    self.description = description
-    self.location = location
-
-rocks = [
-  Rock('red', 'shiny', 'beach'),
-  Rock('brown', 'lumpy', 'yard')
-]
-
 def rocks_index(request):
+  rocks = Rock.objects.all()
   return render(request, 'rocks/index.html', {'rocks': rocks})
