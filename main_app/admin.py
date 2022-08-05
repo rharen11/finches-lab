b3f12fd72372c3691d7c59dbf994d7624aee1288
Rachel Kinzel <rachel.l.kinzel@gmail.com>
2022-08-05 15:47:05 -0700

    part 2

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Rock
 
-# Register your models here.
+admin.site.register(Rock)
--- main_app/views.py
+++ main_app/views.py
@@ -10,3 +10,7 @@ def about(request):
 def rocks_index(request):
   rocks = Rock.objects.all()
   return render(request, 'rocks/index.html', {'rocks': rocks})
+
+def rocks_detail(request, rock_id):
+  rock = Rock.objects.get(id=rock_id)
+  return render(request, 'rocks/detail.html', {'rock': rock})
