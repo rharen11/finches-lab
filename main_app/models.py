d274623a3cd6d56c144c44b828e950d42f65f743
Rachel Kinzel <rachel.l.kinzel@gmail.com>
2022-08-06 14:20:36 -0700

    edit functioning

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Rock(models.Model):
   type = models.CharField(max_length=100)
@@ -8,3 +9,7 @@ class Rock(models.Model):
 
   def __str__(self):
     return self.type
+
+  def get_absolute_url(self):
+      return reverse("rocks_detail", kwargs={"rock_id": self.id})
+  
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Rock
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 def home(request):
   return render(request, 'home.html')
@@ -14,3 +15,16 @@ def rocks_index(request):
 def rocks_detail(request, rock_id):
   rock = Rock.objects.get(id=rock_id)
   return render(request, 'rocks/detail.html', {'rock': rock})
+
+class RockCreate(CreateView):
+  model = Rock
+  fields = '__all__'
+  success_url= '/rocks/'
+
+class RockUpdate(UpdateView):
+  model = Rock
+  fields= ['type', 'color', 'description', 'location']
+
+class RockDelete(DeleteView):
+  model = Rock
+  success_url= '/rocks/'
